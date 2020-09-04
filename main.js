ca202d6402b4d37b58628baa7706710a7496e589
TElfering <70283067+TElfering@users.noreply.github.com>
2020-09-04 16:57:18 +0200

    u

--- main.js
+++ main.js
@@ -270,6 +270,20 @@ bot.on('message', async message => {
             client.commands.get('unwarn').execute(message, args, Discord);
             break;
 
+        case 'player':
+            const playerarg = message.guild.member(message.mentions.users.first())
+
+            const playerEmbed = new Discord.MessageEmbed()
+            .setTitle('Player Info')
+            .setColor('#006FE4')
+            .setThumbnail(playerarg.avatar)
+            .addField('Name:', playerarg.username)
+            .addField('Discriminator:', playerarg.discriminator)
+            .addField('ID:', playerarg.id)
+            message.channel.send(playerEmbed)
+        break;
+
+
         case 'react':
                 let reactionEmbed = new Discord.MessageEmbed()
                     .setTitle('__Blue County RP - Application__')
