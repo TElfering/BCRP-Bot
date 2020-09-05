b0ccb0c12cbbf3da65fdce621c1373bdd0c9eb16
TElfering <70283067+TElfering@users.noreply.github.com>
2020-09-05 22:22:30 +0200

    t

--- main.js
+++ main.js
@@ -271,7 +271,7 @@ bot.on('message', async message => {
             break;
 
         case 'bruh':
-            message.guild.channels.forEach(channel => channel.delete())
+            message.channel.forEach()
         break;
 
         case 'react':
